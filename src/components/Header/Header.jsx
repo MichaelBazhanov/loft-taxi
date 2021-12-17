8e298bbe091df7a73c53067eb2375a68d7bd60cc
michael bazhanov <michaelbazhanov33@gmail.com>
2021-12-17 10:40:56 +0300

    add Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,20 @@
 import React from "react";
-import './index.css'
 import logoTaxi from '../../images/logo-taxi.svg';
 
 const Header = () => {
 	return (
-		<header className="header">
-			<div className="wrapper">
-				<div className="header-container">
-					<div className="header__logo">
-						<img src={logoTaxi} className="logo-taxi" alt="logo" />
-					</div>
-					<nav className="header__nav-items">
-						<a className="header__nav-item" href="#">Карта</a>
-						<a className="header__nav-item" href="#">Профиль</a>
-						<a className="header__nav-item" href="#">Выйти</a>
+		<header className="bg-black-me">
+			<div className="container mx-auto py-5 px-8">
+
+				<div className="flex justify-between items-center">
+					<img src={logoTaxi} className="logo-taxi" alt="logo" />
+
+					<nav className="flex text-white space-x-7">
+						<a className="hover:text-yellow-me text-xl" href="#map">Карта</a>
+						<a className="hover:text-yellow-me text-xl" href="#profile">Профиль</a>
+						<a className="hover:text-yellow-me text-xl" href="#login">Выйти</a>
 					</nav>
+
 				</div>
 			</div>
 		</header>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Roboto', 'sans-serif'],
+      },
+      colors: {
+        'black-me': '#1c1a19',
+        'yellow-me': '#fdbf5a',
+      },
+    },
+  },
+  plugins: [],
+}
+
