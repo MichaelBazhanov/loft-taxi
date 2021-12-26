db3f1f4a0f1e2e7da39c03a847f66f28d1afa83c
michael bazhanov <michaelbazhanov33@gmail.com>
2021-12-26 13:21:48 +0300

    add componentn mapbox-gl

--- src/components/FormForMap.jsx
+++ src/components/FormForMap.jsx
@@ -26,59 +26,61 @@ const FormForMap = (props) => {
 	const { active } = state
 
 	return (
-		<div className="flex flex-col">
+		<div className="container mx-auto h-screen relative pointer-events-none">
+			<div className="flex flex-col">
 
-			<form onClick={handleSubmit} className={classNames(active ? 'hidden' : '', 'max-w-[486px] w-full bg-white  mt-16 ml-24 rounded-xl shadow-lg')}>
-				{/* Select что то должен вернуть и я запишу это в инпуты */}
-				<input type="hidden" name="rout-1" />
-				<input type="hidden" name="rout-2" />
+				<form onClick={handleSubmit} className={classNames(active ? 'hidden' : '', 'max-w-[486px] w-full bg-white  mt-16 ml-24 rounded-xl shadow-lg pointer-events-auto')}>
+					{/* Select что то должен вернуть и я запишу это в инпуты */}
+					<input type="hidden" name="rout-1" />
+					<input type="hidden" name="rout-2" />
 
-				<div className="p-6 pb-0">
-					<Select />
-					<Select />
-				</div>
-
-				<hr className="border w-full" />
-
-				<div className="py-8 px-10 mt-6 rounded-xl border">
-					<div className="flex -mx-3">
-						<div className="w-1/3 px-3 cursor-pointer">
-							<div className="flex flex-col shadow-me rounded-lg p-3">
-								<p>Стандарт</p>
-								<p className="text-xs">Стоимость</p>
-								<p className="text-2xl">150 ₽</p>
-								<img src={imgStandart} alt="car" className="w-full h-auto" />
-							</div>
-						</div>
-						<div className="w-1/3 px-3 cursor-pointer opacity-50">
-							<div className="flex flex-col shadow-me rounded-lg p-3">
-								<p>Премиум</p>
-								<p className="text-xs">Стоимость</p>
-								<p className="text-2xl">250 ₽</p>
-								<img src={imgPremium} alt="car" className="w-full h-auto" />
-							</div>
-						</div>
-						<div className="w-1/3 px-3 cursor-pointer opacity-50">
-							<div className="flex flex-col shadow-me rounded-lg p-3">
-								<p>Бизнес</p>
-								<p className="text-xs">Стоимость</p>
-								<p className="text-2xl">300 ₽</p>
-								<img src={imgBusiness} alt="car" className="w-full h-auto" />
-							</div>
-						</div>
+					<div className="p-6 pb-0">
+						<Select />
+						<Select />
 					</div>
-					{/* верхний блок что то должен вернуть и я запишу это в инпуты */}
-					<input type="hidden" name="car" />
-					<button onClick={() => { handleChange(true) }} type="submit" className="text-2xl py-4 w-full bg-yellow-me rounded-full mt-7">Заказать</button>
+
+					<hr className="border w-full" />
+
+					<div className="py-8 px-10 mt-6 rounded-xl border">
+						<div className="flex -mx-3">
+							<div className="w-1/3 px-3 cursor-pointer">
+								<div className="flex flex-col shadow-me rounded-lg p-3">
+									<p>Стандарт</p>
+									<p className="text-xs">Стоимость</p>
+									<p className="text-2xl">150 ₽</p>
+									<img src={imgStandart} alt="car" className="w-full h-auto" />
+								</div>
+							</div>
+							<div className="w-1/3 px-3 cursor-pointer opacity-50">
+								<div className="flex flex-col shadow-me rounded-lg p-3">
+									<p>Премиум</p>
+									<p className="text-xs">Стоимость</p>
+									<p className="text-2xl">250 ₽</p>
+									<img src={imgPremium} alt="car" className="w-full h-auto" />
+								</div>
+							</div>
+							<div className="w-1/3 px-3 cursor-pointer opacity-50">
+								<div className="flex flex-col shadow-me rounded-lg p-3">
+									<p>Бизнес</p>
+									<p className="text-xs">Стоимость</p>
+									<p className="text-2xl">300 ₽</p>
+									<img src={imgBusiness} alt="car" className="w-full h-auto" />
+								</div>
+							</div>
+						</div>
+						{/* верхний блок что то должен вернуть и я запишу это в инпуты */}
+						<input type="hidden" name="car" />
+						<button onClick={() => { handleChange(true) }} type="submit" className="text-2xl py-4 w-full bg-yellow-me rounded-full mt-7">Заказать</button>
+					</div>
+				</form>
+
+				<div className={classNames(active ? '' : 'hidden', 'max-w-[486px] w-full bg-white  mt-16 ml-24 rounded-xl shadow-lg py-10 px-11 pointer-events-auto')}>
+					<p className="font-bold text-4xl">Заказ размещен</p>
+					<p className="mt-4 text-lg text-gray-me">Ваше такси уже едет к вам. Прибудет приблизительно через 10 минут.</p>
+					<button onClick={() => { handleChange(false) }} type="submit" className="text-2xl py-4 w-full bg-yellow-me rounded-full mt-7">Сделать новый заказ</button>
 				</div>
-			</form>
 
-			<div className={classNames(active ? '' : 'hidden', 'max-w-[486px] w-full bg-white  mt-16 ml-24 rounded-xl shadow-lg py-10 px-11')}>
-				<p className="font-bold text-4xl">Заказ размещен</p>
-				<p className="mt-4 text-lg text-gray-me">Ваше такси уже едет к вам. Прибудет приблизительно через 10 минут.</p>
-				<button onClick={() => { handleChange(false) }} type="submit" className="text-2xl py-4 w-full bg-yellow-me rounded-full mt-7">Сделать новый заказ</button>
 			</div>
-
 		</div>
 	)
 }
--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,18 +1,57 @@
-import React from "react";
+import React, { Component } from "react";
+import mapboxGl from "mapbox-gl";
 
 import FormForMap from '../components/FormForMap'
 
-const Map = (props) => {
+class Map extends Component {
+	map = null
+	mapContainer = React.createRef(); // создаем ссылку
 
-	return (
-		<div className="bg-map bg-center">
-			<div className="container mx-auto h-screen ">
+	componentDidMount() {
+		mapboxGl.accessToken = 'pk.eyJ1IjoibWljaGFlbDMzIiwiYSI6ImNreG4xYWVycjA0NDgybm9lbW55ZTN6d2EifQ.v5o2tFYQgax6FxvwN-Vd1g'
 
-				<FormForMap />
+		this.map = new mapboxGl.Map({ // создаем карту и вкладываем настройки
+			container: this.mapContainer.current, // это ссылка на элемент которому мы передали референс
+			style: 'mapbox://styles/mapbox/streets-v11', // style URL
+			center: [40.41667, 56.13333], // starting position [lng, lat]
+			zoom: 12 // starting zoom
+		})
+	}
 
+	componentWillUnmount() {
+		this.map.remove(); // удаляем карту
+	}
+
+	render() {
+
+		return (
+			// <div className="bg-map bg-center">
+			// 	<div className="container mx-auto h-screen ">
+
+			// 		{/* <FormForMap /> */}
+
+			// 		<div className="map-wrapper">
+			// 			<div
+			// 				data-testid='map'
+			// 				className="map"
+			// 				ref={this.mapContainer}
+			// 			>
+			// 			</div>
+			// 		</div>
+
+			// 	</div>
+			// </div>
+			<div className="relative">
+				<div
+					data-testid='map'
+					className="absolute inset-0"
+					ref={this.mapContainer}
+				>
+				</div>
+					<FormForMap />
 			</div>
-		</div>
-	)
+		)
+	}
 }
 
 export default Map
