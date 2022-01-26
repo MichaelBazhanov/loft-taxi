a50f642d797dbb7fc4c454ea0a46ca3852490829
michael bazhanov <michaelbazhanov33@gmail.com>
2022-01-26 22:13:34 +0300

    add routesSaga.spec.js

---
+++ src/__specs__/routesSaga.spec.js
@@ -0,0 +1,49 @@
+import { recordSaga } from '../recordSaga'
+import { coordinates } from '../modules/route'
+import { serverGetRoutes } from '../api'
+
+//Это ACTION с которого начинается исполнение SAGA (this action creator)
+import { getRoutesCoordinates } from '../modules/route'
+
+// jest.mock('../api', () => ({ serverLogin: jest.fn(() => true) }))
+jest.mock('../api') //Мы мокаем api для того что бы была доступна функция serverLogin внутри теста
+
+describe('coordinates', () => {
+  describe('#GET_ROUTES_COORDINATES', () => {
+
+    it('Получение координат через api', async () => {
+      serverGetRoutes.mockImplementation(async () => ([['1','2'],['3','4'],['5','6']]))
+
+      const dispatched = await recordSaga(
+        coordinates, // сама сага
+        getRoutesCoordinates(), // action на который тригерим эту сагу
+      )
+
+      expect(dispatched).toEqual([
+        {
+          type: 'ROUTES_COORDINATES_SUCCESS',
+          payload: {
+            coordinates: [['1','2'],['3','4'],['5','6']]
+          },
+        },
+      ])
+    })
+    // ===============================================================================================
+    it('Не получение координат через api', async () => {
+
+      serverGetRoutes.mockImplementation(async () => ({}))
+
+      const dispatched = await recordSaga(
+        coordinates, // сама сага
+        getRoutesCoordinates(), // action на который тригерим эту сагу
+      )
+
+      expect(dispatched).toEqual([
+        {
+          type: 'ROUTES_COORDINATES_FAILURE',
+          payload: { error: 'error' },
+        },
+      ])
+    })
+  })
+})
--- src/modules/route/routesSaga.js
+++ src/modules/route/routesSaga.js
@@ -10,9 +10,14 @@ export function* coordinates(action) {
   try {
     const { address1, address2 } = action.payload //вынимаем данные из ACTION
     const coordinates = yield call(serverGetRoutes, address1, address2) // получаем координаты по 2ум адресам
-    yield put(routesCoordinatesSuccess(coordinates))
+
+    if (Array.isArray(coordinates) && coordinates.length > 0 ) {
+      yield put(routesCoordinatesSuccess(coordinates))
+    } else {
+      yield put(routesCoordinatesFailure(new Error('error').message))
+    }
   } catch (error) {
-    console.error(error.message)
+    // console.error(error.message)
     yield put(routesCoordinatesFailure(error.response))
   }
 }
