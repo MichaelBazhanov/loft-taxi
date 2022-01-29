3c0e2282a1d842c5ba5d4beac631c7c3e94ca9eb
michael bazhanov <michaelbazhanov33@gmail.com>
2022-01-29 19:15:17 +0300

    add test Login

--- src/__test__/LogIn.spec.js
+++ src/__test__/LogIn.spec.js
@@ -2,8 +2,11 @@ import React from 'react'
 import TestRenderer from 'react-test-renderer'
 import { Provider } from 'react-redux'
 
-import Login from '../pages/Login'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
+import Login from '../pages/Login'
+// ======================================================================================= Тестирование SNAPSHOTS
 // Мокаем useNavigate что бы внутри <Login /> отдал просто функцию (ПО СУТИ МЫ МОКАЕМ ХУК)
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
@@ -13,9 +16,11 @@ jest.mock('react-router-dom', () => ({
 const mockStore = {
   getState: () => ({
     // входящие пропсы
-    authorizationReducer: { isLoggedIn: true },
-    authorizationReducer: { isLoading: false },
-    authorizationReducer: { error: false },
+    authorizationReducer: {
+      isLoggedIn: true,
+      isLoading: false,
+      error: false,
+    },
   }),
   subscribe: () => {},
   dispatch: () => {
@@ -33,3 +38,40 @@ it('Renders correctly', () => {
 
   expect(LoginExample).toMatchSnapshot()
 })
+// ======================================================================================= Тестирование SNAPSHOTS
+
+// ======================================================================================= Тестирование библиотекой тестирования
+describe('Login', () => {
+  test('При вводе данных в input на form происходят изменения в state компонента', () => {
+    // 1 Найти на форме inputs
+    // 2 Вписать туда данные
+    // 3 Проверить что эти данные в state появились
+    // const MockComponentLogin = () => {
+    //   <Provider store={mockStore}>
+    //     <Login />
+    //   </Provider>
+    // }
+
+    render(
+      <Provider store={mockStore}>
+        <Login />
+      </Provider>,
+    )
+    // render(<Login />)
+
+    const mail = screen.getByPlaceholderText('mail@mail.ru')
+    // const password = screen.getByPlaceholderText('*************')
+
+    userEvent.type(mail, 'Текст для mail')
+    expect(mail).toHaveValue('Текст для mail')
+  })
+
+  // test('При клики на кнопку submit на форме сама форма успешно отправляется', () => {
+  //   const MockComponentLogin = () => {
+  //     <Login />
+  //   }
+  // const onChange = jest.fn()
+  // expect(onChange).toHaveBeenCalled(2)
+  // })
+})
+// ======================================================================================= Тестирование библиотекой тестирования
