0c0fed2ea64216e9acbf325025d517fada695b07
michael bazhanov <michaelbazhanov33@gmail.com>
2022-01-30 16:32:40 +0300

    add validate component Ligin

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,7 +10,8 @@ import Loading from '../components/Loading'
 import Error from '../components/Error'
 
 const Login = ({ authenticate, isLoggedIn, isLoading, error }) => {
-	const [state, setState] = useState({ email: 'test@test.com', password: '123123' }) // hard code
+	// const [state, setState] = useState({ email: 'test@test.com', password: '123123' }) // hard code
+	// const [state, setState] = useState({ email: '', password: '' })
 
 	const navigate = useNavigate()
 
@@ -18,18 +19,17 @@ const Login = ({ authenticate, isLoggedIn, isLoading, error }) => {
 		isLoggedIn ? navigate("/map", { replace: true }) : navigate("/login", { replace: true })
 	}, [isLoggedIn])
 
-	const handleSubmit = (event) => {
-		event.preventDefault()
+	// const handleSubmit = (event) => {
+	// 	event.preventDefault()
+	// 	const { email, password } = state // получаем из state
+	// 	authenticate(email, password) // отдаем данные для авторизации
+	// }
 
-		const { email, password } = state // получаем из state
-		authenticate(email, password) // отдаем данные для авторизации
-	}
+	// const handleChange = (event) => { console.log('handleChange good')
+	// 	setState({ ...state, [event.target.name]: event.target.value })
+	// }
 
-	const handleChange = (event) => {
-		setState({ ...state, [event.target.name]: event.target.value })
-	}
-
-	const { email, password } = state
+	// const { email, password } = state
 
 	if (isLoading) return <Loading />
 	if (error) return <Error error={error} />
@@ -47,68 +47,111 @@ const Login = ({ authenticate, isLoggedIn, isLoading, error }) => {
 			<div
 				className="w-2/3 flex justify-center items-center"
 			>
-				<form
-					onSubmit={handleSubmit} className="max-w-xl w-full bg-white px-28 py-14 shadow-lg rounded-2xl" autoComplete="off">
-					<h4
-						className="font-bold text-3xl text-black text-center mb-14">
-						Войти
-					</h4>
+				<Formik
+					onSubmit={(values, FormikBag) => {
+						// console.log('Formik сработал  onSubmit', values, FormikBag)
+						const { email, password } = values
+						authenticate(email, password)
 
-					<label
-						className="flex flex-col justify-between font-bold cursor-pointer">
-						<span>Email*</span>
-						<input
-							onChange={handleChange}
-							value={email}
-							type="email"
-							name="email"
-							required
-							autoComplete="off"
-							placeholder="mail@mail.ru"
-							className="mt-1 block w-full border-b-2 border-gray-300 shadow-sm
-						focus:outline-none focus:border-yellow-me focus:ring-yellow-me focus:placeholder-yellow-me
-						placeholder:text-gray-me"/>
-					</label>
+						FormikBag.setSubmitting(true) // защита от повторной отправки
+					}}
+					initialValues={{ email: "test@test.com", password: "123123" }}
+					validate={(value) => {
+						console.log('Formik - validate - value', value)
+						let errors = {}
+						if (!value.email) {
+							errors.email = 'Required';
+						} else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value.email)) {
+							errors.email = 'Invalid email address';
+						}
+						if (value.password.length < 5) {
+							errors.password = 'Password must be longer than 5 characters!'
+						}
+						return errors
+					}}
 
-					<label className="flex flex-col justify-between font-bold cursor-pointer mt-6">
-						<span>Password*</span>
-						<input
-							onChange={handleChange}
-							value={password}
-							type="password"
-							name="password"
-							required
-							autoComplete="off"
-							placeholder="*************"
-							className="mt-1 block w-full border-b-2 border-gray-300 shadow-sm
-						focus:outline-none focus:border-yellow-me focus:ring-yellow-me focus:placeholder-yellow-me
-						placeholder:text-gray-me"/>
-					</label>
 
-					<p
-						className="mt-3 text-right">
-						<span
-							className="inline-block text-gray-me cursor-pointer hover:text-yellow-me">
-							Забыли пароль?
-						</span>
-					</p>
+				>
+					{({
+						values,
+						errors,
+						touched,
+						handleChange,
+						handleBlur,
+						handleSubmit,
+						isValid,
+						isSubmitting,
+					}) => (
+						<form
+							onSubmit={handleSubmit} className="max-w-xl w-full bg-white px-28 py-14 shadow-lg rounded-2xl">
+							<h4
+								className="font-bold text-3xl text-black text-center mb-14">
+								Войти
+							</h4>
 
-					<button
-						type="submit"
-						className="block text-center cursor-pointer mt-11 bg-yellow-me w-full py-4 text-2xl rounded-full">
-						Войти
-					</button>
+							<label
+								className="flex flex-col justify-between font-bold cursor-pointer">
+								<span>Email*</span>
+								<input
+									type="email"
+									name="email"
+									onChange={handleChange}
+									onBlur={handleBlur}
+									value={values.email}
+									required
+									autoComplete="off"
+									placeholder="mail@mail.ru"
+									className="mt-1 block w-full border-b-2 border-gray-300 shadow-sm
+		focus:outline-none focus:border-yellow-me focus:ring-yellow-me focus:placeholder-yellow-me
+		placeholder:text-gray-me"/>
+								{touched.email && errors.email && <div className="text-red-600">{errors.email}</div>}
+							</label>
 
-					<p
-						className="mt-8 text-gray-me text-center">
-						Новый пользователь?&nbsp;
-						<span
-							onClick={() => { navigate("/registration") }}
-							className="inline-block text-yellow-me cursor-pointer">
-							Регистрация
-						</span>
-					</p>
-				</form>
+							<label className="flex flex-col justify-between font-bold cursor-pointer mt-6">
+								<span>Password*</span>
+								<input
+									type="password"
+									name="password"
+									onChange={handleChange}
+									onBlur={handleBlur}
+									value={values.password}
+									required
+									autoComplete="off"
+									placeholder="*************"
+									className="mt-1 block w-full border-b-2 border-gray-300 shadow-sm
+		focus:outline-none focus:border-yellow-me focus:ring-yellow-me focus:placeholder-yellow-me
+		placeholder:text-gray-me"/>
+								{touched.password && errors.password && <div className="text-red-600">{errors.password}</div>}
+							</label>
+
+							<p
+								className="mt-3 text-right">
+								<span
+									className="inline-block text-gray-me cursor-pointer hover:text-yellow-me">
+									Забыли пароль?
+								</span>
+							</p>
+
+							<button
+								disabled={isSubmitting || !isValid}
+								type="submit"
+								className="block text-center cursor-pointer mt-11 bg-yellow-me w-full py-4 text-2xl rounded-full disabled:opacity-75 disabled:cursor-default">
+								Войти
+							</button>
+
+							<p
+								className="mt-8 text-gray-me text-center">
+								Новый пользователь?&nbsp;
+								<span
+									onClick={() => { navigate("/registration") }}
+									className="inline-block text-yellow-me cursor-pointer">
+									Регистрация
+								</span>
+							</p>
+						</form>
+					)}
+
+				</Formik>
 			</div>
 		</div>
 	)
