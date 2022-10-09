2537087849c0ab34de4b4f7b8cfcf7786d868a4f
michael bazhanov <michaelbazhanov33@gmail.com>
2022-10-09 14:00:21 +0300

    fix test HOCMapContainer = MapCOntainer

--- src/__test__/MapContainer.spec.js
+++ src/__test__/MapContainer.spec.js
@@ -1,33 +1,33 @@
-import React from 'react'
-import TestRenderer from 'react-test-renderer'
-import { Provider } from 'react-redux'
+import React from "react";
+import TestRenderer from "react-test-renderer";
+import { Provider } from "react-redux";
 
-import MapContainer from '../components/MapContainer'
+import { HOCMapContainer } from "../components/MapContainer";
 
-// Мокаем useNavigate что бы внутри <MapContainer /> отдал просто функцию
-jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom'),
+// Мокаем useNavigate что бы внутри <HOCMapContainer /> отдал просто функцию
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
   useNavigate: () => jest.fn(),
-}))
+}));
 // Формируем замоканый стор
 const mockStore = {
   getState: () => ({
     // входящие пропсы
-    authorizationReducer: { token: '123' },
+    authorizationReducer: { token: "123" },
 
     addressReducer: {
       address: [
-        { id: 1, root: 'Адрес 1' },
-        { id: 2, root: 'Адрес 2' },
+        { id: 1, root: "Адрес 1" },
+        { id: 2, root: "Адрес 2" },
       ],
       isLoading: false,
       error: false,
     },
     paymentReducer: {
-      cardName: 'test',
-      cardNumber: '1',
-      expiryDate: '000',
-      cvc: '1',
+      cardName: "test",
+      cardNumber: "1",
+      expiryDate: "000",
+      cvc: "1",
       isLoadingSendPaymentCardNewUser: false,
       errorSendPaymentCardNewUser: false,
     },
@@ -35,18 +35,18 @@ const mockStore = {
   subscribe: () => {},
   dispatch: () => {
     // диспачим внутри компонента
-    getAddressList: jest.fn()
-    getRoutesCoordinates: jest.fn()
-    getPaymentCard: jest.fn()
+    getAddressList: jest.fn();
+    getRoutesCoordinates: jest.fn();
+    getPaymentCard: jest.fn();
   },
-}
+};
 
-it('Renders correctly', () => {
+it("Renders correctly", () => {
   const FormForMapExample = TestRenderer.create(
     <Provider store={mockStore}>
-      <MapContainer />
-    </Provider>,
-  ).toJSON()
+      <HOCMapContainer />
+    </Provider>
+  ).toJSON();
 
-  expect(FormForMapExample).toMatchSnapshot()
-})
+  expect(FormForMapExample).toMatchSnapshot();
+});
